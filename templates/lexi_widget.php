c547600eaed6b3164a3d6803fce8bf3aecf6b2db
sebaxtian <sebaxtian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-18 14:01:15 +0000

    Versión 0.7.4.1
    git-svn-id: https://plugins.svn.wordpress.org/lexi/tags/0.7.4.1@118699 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/lexi_widget.php
@@ -0,0 +1,20 @@
+<p>
+	<label for="lexi_title">
+		<?php _e('Title', 'lexi'); ?>:
+		<input class="widefat" id="lexi_title" name="lexi_title" type="text" value="<?php echo $title; ?>" />
+	</label>
+</p>
+<p>
+	<label>
+		<?php printf(__('You can <a href="%s">modify</a> the feeds list in <strong>Tools/Lexi</strong>', 'lexi'), "tools.php?page=leximanage"); ?>
+	</label>
+</p>
+<?php
+if(!function_exists('minimax')) { ?>
+<p>
+	<label>
+		<?php printf(__('You have to install <a href="%s"  target="_BLANK">minimax 0.2</a> in order for this plugin to work', 'lexi'), "http://wordpress.org/extend/plugins/minimax/" ); ?>
+	</label>
+</p><?
+} ?>
+<input type="hidden" id="lexi-submit" name="lexi-submit" value="1" />
